b60ee92c676320641bd6fd367f8bd576e9f7b58b
ArashiNilou <arash.niloufary@yahoo.com>
2025-02-26 14:47:38 +0100

    Fix long line issue in check_commit.py

--- .github/check_commit.py
+++ .github/check_commit.py
@@ -13,7 +13,8 @@ commit_regex = r"^[A-Z].{0,50}[^.]$"
 
 def check_pr_message():
     """Do basic checks on commit message sanity."""
-    with open(os.environ["GITHUB_EVENT_PATH"], "r", encoding="utf-8") as fp_git_event:
+    with open(os.environ["GITHUB_EVENT_PATH"], "r", encoding="utf-8"
+) as fp_git_event:
         event = fp_git_event.read()
     event_json = json.loads(event)
     head_commit_sha = event_json["pull_request"]["base"]["sha"]
